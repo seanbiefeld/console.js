b208806f4b5ca8cb9bbbfa3f9663a28f3df6598a
Sean Biefeld <sbiefeld@gmail.com>
2011-10-06 11:14:54 -0500

    added history use up and down arrows to go through it

--- source/console.js
+++ source/console.js
@@ -1,11 +1,16 @@
 var Console = {
 
 	code: [],
+	history: [],
+	historyPosition: Number.NaN,
 	clearCode: function () {
 		"use strict";
 		this.code.splice(0, this.code.length);
 	},
-
+	clearHistory: function () {
+		"use strict";
+		this.history.splice(0, this.history.length);
+	},
 	evaluateCode: function (codeToRun) {
 		"use strict";
 		var func = new Function(codeToRun);
@@ -19,7 +24,7 @@ var Console = {
 		$("<pre></pre>").addClass("history").appendTo(console);
 		$("<div></div>").html("&gt;").addClass("caret").appendTo(console);
 		inputContainer = $("<div></div>").addClass("inputContainer").appendTo(console);
-		$("<input></input>").attr("placeholder", "start scripting here...").addClass("input").keypress(this.codeEntered).appendTo(inputContainer).focus();
+		$("<input></input>").attr("placeholder", "start scripting here...").addClass("input").keyup(this.codeEntered).appendTo(inputContainer).focus();
 	},
 
 	isStatement: function (value) {
@@ -35,19 +40,42 @@ var Console = {
 	},
 	codeEntered: function (e) {
 		"use strict";
+		var input, console, history, evaluation, inputValue;
+		input = $("#console input");
+		console = $("#console");
+		history = $("#console pre");
+		evaluation = "nothing";
+		inputValue = input.val();
+		
+		//go back in history
+		if ((e.which && e.which === 38) || (e.keyCode && e.keyCode === 38)) {
+			if (isNaN(Console.historyPosition)) {
+				Console.historyPosition = Console.history.length - 1;
+				input.val(Console.history[Console.historyPosition]);
+			} else {				
+				if (Console.historyPosition > -1) {
+					Console.historyPosition--;
+					input.val(Console.history[Console.historyPosition]);					
+				}
+			}
+		}
+		
+		//go forward in history
+		if ((e.which && e.which === 40) || (e.keyCode && e.keyCode === 40)) {
+			if (Console.historyPosition < Console.history.length) {
+				Console.historyPosition++;
+				input.val(Console.history[Console.historyPosition]);				
+			}
+		}
+		
 		if ((e.which && e.which === 13) || (e.keyCode && e.keyCode === 13)) {
-			var input, console, history, evaluation, inputValue;
-			input = $("#console input");
-			console = $("#console");
-			history = $("#console pre");
-			evaluation = "nothing";
-			inputValue = input.val();
-
 			history.append("&gt;&gt; " + inputValue.replace(/>/gi, "&gt;").replace(/</gi, "&lt;") + "<br />");
-
+			Console.history.push(inputValue);
+			
 			switch (inputValue.toUpperCase()) {
 			case "CLEAR":
 				Console.clearCode();
+				Console.clearHistory();
 				history.append("&gt;&gt; " + inputValue.replace(/>/gi, "&gt;").replace(/</gi, "&lt;") + "<br />");
 				break;
 
@@ -66,6 +94,7 @@ var Console = {
 					var isAssignment = inputValue.indexOf("=") >= 0;
 
 					if (isVariableDeclaration || isFunction || isAssignment) {
+						Console.evaluateCode(inputValue);
 						Console.code.push("\r\r " + inputValue);
 					} else {
 						for (var i = 0; i < Console.code.length; i++) {
@@ -88,7 +117,7 @@ var Console = {
 
 					evaluation = Console.evaluateCode(codeToRun);
 
-					if (evaluation || evaluation === 0) {
+					if (evaluation || evaluation === 0 || evaluation === false) {
 						history.append('<span style="color:#8F9D6A">=&gt; ' + evaluation + "</span><br/>");
 					}
 					
